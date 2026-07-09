aa61ed9c246b11356177e318b95e015194ec6b82
Coen ten Thije Boonkkamp <coen@coenttb.com>
2026-07-09 11:06:22 +0200

    Restructure test suites to [SWIFT-TEST-002] extension pattern (1 suite)

--- Tests/Comparison Finite Primitives Tests/Enumerable Tests.swift
+++ Tests/Comparison Finite Primitives Tests/Enumerable Tests.swift
@@ -1,12 +1,14 @@
 import Comparison_Finite_Primitives
 import Testing
 
-@Suite("Enumerable Tests")
-struct EnumerableTests {
-    @Suite struct Unit {}
+extension Comparison {
+    @Suite("Enumerable Tests")
+    struct Test {
+        @Suite struct Unit {}
+    }
 }
 
-extension EnumerableTests.Unit {
+extension Comparison.Test.Unit {
     @Test
     func `count is three`() {
         #expect(Comparison.count == 3)
